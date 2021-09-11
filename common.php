6b12848eb5ec91c957a77910a982b7d7d2950624
Rafael <rafa.xt@protonmail.com>
2021-09-11 18:11:55 -0300

    +função para fazer consulta nas réplicas

--- common.php
+++ common.php
@@ -506,4 +506,25 @@ function resolveRedir($page){
 	}
 }
 
+// Função para fazer consultas nas réplicas no Toolforge
+function replicaQuery($project, $query, $param){
+
+	global $toolforge;
+
+	if($toolforge===0){
+		exit(logging("O script requer acesso ao replicas mas não parece estar rodando no Toolforge, verifique. Fechando...\r\n"));
+	}
+
+	$ts_pw = posix_getpwuid(posix_getuid());
+	$ts_mycnf = parse_ini_file($ts_pw['dir'] . "/replica.my.cnf");
+	$mysqli = new mysqli($project . '.analytics.db.svc.wikimedia.cloud', $ts_mycnf['user'], $ts_mycnf['password'], $project . '_p');
+	unset($ts_mycnf, $ts_pw);
+
+	$stmt = $mysqli->prepare($query);
+	$stmt->bind_param('s', $param);
+	$stmt->execute();
+	$result = $stmt->get_result();
+	return $result->fetch_all(MYSQLI_BOTH);
+}
+
  ?>
