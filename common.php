cff86659ffb442cdc05ff2b3a721f40d11ec9954
Rafael <rafa.xt@protonmail.com>
2021-09-14 11:59:15 -0300

    fix função réplicas: retornar 0 quando $toolforge=0

--- common.php
+++ common.php
@@ -520,20 +520,23 @@ function replicaQuery($project, $query, $param){
 
 	global $toolforge;
 
-	if($toolforge===0){
-		exit(logging("O script requer acesso ao replicas mas não parece estar rodando no Toolforge, verifique. Fechando...\r\n"));
+	if($toolforge===1){
+
+		$ts_pw = posix_getpwuid(posix_getuid());
+		$ts_mycnf = parse_ini_file($ts_pw['dir'] . "/replica.my.cnf");
+		$mysqli = new mysqli($project . '.analytics.db.svc.wikimedia.cloud', $ts_mycnf['user'], $ts_mycnf['password'], $project . '_p');
+		unset($ts_mycnf, $ts_pw);
+
+		$stmt = $mysqli->prepare($query);
+		$stmt->bind_param('s', $param);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		return $result->fetch_all(MYSQLI_BOTH);
+
+	}else{
+		return 0;
 	}
 
-	$ts_pw = posix_getpwuid(posix_getuid());
-	$ts_mycnf = parse_ini_file($ts_pw['dir'] . "/replica.my.cnf");
-	$mysqli = new mysqli($project . '.analytics.db.svc.wikimedia.cloud', $ts_mycnf['user'], $ts_mycnf['password'], $project . '_p');
-	unset($ts_mycnf, $ts_pw);
-
-	$stmt = $mysqli->prepare($query);
-	$stmt->bind_param('s', $param);
-	$stmt->execute();
-	$result = $stmt->get_result();
-	return $result->fetch_all(MYSQLI_BOTH);
 }
 
  ?>
