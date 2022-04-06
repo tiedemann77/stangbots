e58147f7ff03a95760212ce21c05e4f3bbaa4657
Rafael <rafa.xt@protonmail.com>
2022-04-06 12:28:07 -0300

    autoloader.php: ajustes

--- autoloader.php
+++ autoloader.php
@@ -6,12 +6,12 @@ spl_autoload_register(function($class) {
 	$file = __DIR__ . "/src/" . $class . ".php";
 
 	if(file_exists( $file )) {
-		require_once $file;
+		require_once($file);
 	}
 
 });
 
 // Carregando regex comuns
-require_once("regex.php")
+require_once("regex.php");
 
 ?>
