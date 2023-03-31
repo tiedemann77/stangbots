565d9ff60c962f94374a3cede85f784dea034ab7
Rafael <rafa.xt@protonmail.com>
2023-03-31 15:52:12 -0300

    Add files via upload

---
+++ scripts/mais-teoria/autoloader.php
@@ -0,0 +1,14 @@
+<?php
+
+// Carregando classes
+spl_autoload_register(function($class) {
+
+	$file = __DIR__ . "/src/" . $class . ".php";
+
+	if(file_exists( $file )) {
+		require_once($file);
+	}
+
+});
+
+?>
